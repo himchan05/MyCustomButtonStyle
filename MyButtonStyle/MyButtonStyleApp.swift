cf49bd2bfbbcfbb36dd945062c0b9867538d90a1
박힘찬 <himchan05@naver.com>
2022-03-14 20:43:18 +0900

    Initial Commit

---
+++ MyButtonStyle/MyButtonStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyButtonStyleApp.swift
+//  MyButtonStyle
+//
+//  Created by 박힘찬 on 2022/03/14.
+//
+
+import SwiftUI
+
+@main
+struct MyButtonStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
